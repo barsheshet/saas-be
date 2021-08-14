c6041a823b36fd420e8075a7bcb33022e2c379d6
barsheshet <guybarsheshet1@gmail.com>
2021-08-14 20:51:37 +0300

    Second commit

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,9 @@ import { join } from 'path';
 import AutoLoad, { AutoloadPluginOptions } from 'fastify-autoload';
 import { FastifyPluginAsync } from 'fastify';
 
-export type AppOptions = {
-  // Place your custom options for app below here.
-} & Partial<AutoloadPluginOptions>;
+// export type AppOptions = {
+//   // Place your custom options for app below here.
+// } & Partial<AutoloadPluginOptions>;
 
 const app: FastifyPluginAsync<AppOptions> = async (fastify, opts): Promise<void> => {
   // Place here your custom code!
